79ddd06e7f026404a5dbd06ae2fd6f53c861edbf
Koziolek <bjkuczynski@gmail.com>
2020-04-17 22:53:27 +0200

    mix lock

---
+++ test/ego/lexer_test.exs
@@ -0,0 +1,34 @@
+defmodule Ego.LexerTest do
+  use ExUnit.Case
+  import Assertions
+  alias Ego.Lexer
+
+  @moduletag :capture_log
+
+  doctest Lexer
+
+  test "()" do
+    result = Lexer.tokenize("()")
+    assert_lists_equal(result, [:open_bracket, :close_bracket, :eof])
+  end
+
+  test "( )" do
+    result = Lexer.tokenize("( )")
+    assert_lists_equal(result, [:open_bracket, :close_bracket, :eof])
+  end
+
+  test "(Print Hello)" do
+    result = Lexer.tokenize("(Print Hello)")
+    assert_lists_equal(result, [:open_bracket, :Print, :Hello, :close_bracket, :eof])
+  end
+
+  test "(Print \"Hello World\")" do
+    result = Lexer.tokenize("(Print \"Hello World\")")
+    assert_lists_equal(result, [:open_bracket, :Print, :"Hello World", :close_bracket, :eof])
+  end
+
+  test "(Print \"Hello ( ) World\")" do
+    result = Lexer.tokenize("(Print \"Hello ( ) World\")")
+    assert_lists_equal(result, [:open_bracket, :Print, :"Hello ( ) World", :close_bracket, :eof])
+  end
+end
