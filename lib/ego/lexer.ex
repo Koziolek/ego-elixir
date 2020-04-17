07b6bf735ad6c9b1a9c7af885c07fe9b3fbb113a
Koziolek <bjkuczynski@gmail.com>
2020-04-18 00:07:44 +0200

    gotowe

--- lib/ego/lexer.ex
+++ lib/ego/lexer.ex
@@ -1,22 +1,38 @@
 defmodule Ego.Lexer do
   def tokenize(program) when is_binary(program) do
     program
-    |> String.split("")
+    |> String.split("", trim: true)
     |> token
   end
 
-  defp token(charlist, accumulator \\ [])
-  defp token([], accumulator), do: accumulator ++ [:eof]
+  defp token(charlist, accumulator \\ [], buffer \\ [], mode \\ :common)
+  defp token([], accumulator, buffer, _), do: accumulator ++ [:eof]
 
-  defp token(charlist, accumulator) do
+  defp token(charlist, accumulator, buffer, :common) do
     [h | t] = charlist
 
     case h do
-      "(" -> token(t, accumulator ++ [:open_bracket])
-      ")" -> token(t, accumulator ++ [:close_bracket])
-      " " -> token(t, accumulator ++ [:close_bracket])
-      _ -> token(t, accumulator)
+      "(" -> token(t, accumulator ++ read_buffer(buffer) ++ [:open_bracket], [])
+      ")" -> token(t, accumulator ++ read_buffer(buffer) ++ [:close_bracket], [])
+      " " -> token(t, accumulator ++ read_buffer(buffer), [])
+      "\"" -> token(t, accumulator ++ read_buffer(buffer), [], :text)
+      _ -> token(t, accumulator, buffer ++ [h])
     end
+  end
+
+  defp token(charlist, accumulator, buffer, :text) do
+    [h | t] = charlist
+
+    case h do
+      "\"" -> token(t, accumulator ++ read_buffer(buffer), [], :common)
+      _ -> token(t, accumulator, buffer ++ [h], :text)
+    end
+  end
+
+  defp read_buffer([]), do: []
+
+  defp read_buffer(buffer) do
+    [buffer |> Enum.join("") |> String.to_atom()]
   end
 
 end
--- test/ego/lexer_test.exs
+++ test/ego/lexer_test.exs
@@ -22,4 +22,22 @@ defmodule Ego.LexerTest do
     assert_lists_equal(result, [:open_bracket, :Print, :Hello, :close_bracket, :eof])
   end
 
+  test "(Print \"Hello World\")" do
+    result = Lexer.tokenize("(Print \"Hello World\")")
+    assert_lists_equal(result, [:open_bracket, :Print, :"Hello World", :close_bracket, :eof])
+  end
+
+  test "(Print \"Hello ( ) World\")" do
+    result = Lexer.tokenize("(Print \"Hello ( ) World\")")
+    assert_lists_equal(result, [:open_bracket, :Print, :"Hello ( ) World", :close_bracket, :eof])
+  end
+
+  test "(Print \"Hello (\n) World\")" do
+    result = Lexer.tokenize("""
+(Print \"Hello (
+    ) World\")
+    """)
+    assert_lists_equal(result, [:open_bracket, :Print, :"Hello (\n) World", :close_bracket, :eof])
+  end
+
 end
