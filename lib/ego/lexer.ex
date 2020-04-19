72c9895e853eaf46cda9d77fa89d3b43166b83aa
Koziolek <bjkuczynski@gmail.com>
2020-04-19 23:12:13 +0200

    Gotowe

--- lib/ego/lexer.ex
+++ lib/ego/lexer.ex
@@ -1,38 +1,37 @@
 defmodule Ego.Lexer do
-
-  alias Ego.Token
+  import Ego.Token
 
   def tokenize(program) when is_binary(program) do
     program
     |> String.to_charlist()
     |> tokens
     |> Enum.reverse()
+    |> List.flatten()
   end
 
   defp tokens(charlist, accumulator \\ [], buffer \\ [], mode \\ :common)
-  defp tokens([], accumulator, _, _), do: [%Token{kind: :eof, value: ''}] ++ accumulator
+  defp tokens([], accumulator, _, _), do: [eof()] ++ accumulator
 
   defp tokens([h | t], accumulator, buffer, :common) do
     cond do
-      '(' === [h] -> tokens(t, [%Token{kind: :open_bracket, value: '('}] ++ read_buffer(buffer, :atom) ++ accumulator, [])
-      ')' === [h] -> tokens(t, [%Token{kind: :close_bracket, value: ')'}] ++ read_buffer(buffer, :atom) ++ accumulator, [])
-      ' ' === [h] -> tokens(t, read_buffer(buffer, :atom) ++ accumulator, [])
-      '"' === [h] -> tokens(t, read_buffer(buffer, :atom) ++ accumulator, [], :text)
+      '(' === [h] -> tokens(t, [open_bracket()] ++ [atom(read_buffer(buffer))] ++ accumulator, [])
+      ')' === [h] -> tokens(t, [close_bracket()] ++ [atom(read_buffer(buffer))] ++ accumulator, [])
+      ' ' === [h] -> tokens(t, [atom(read_buffer(buffer))] ++ accumulator, [])
+      '"' === [h] -> tokens(t, [atom(read_buffer(buffer))] ++ accumulator, [], :text)
       true -> tokens(t, accumulator, [h] ++ buffer)
     end
   end
 
   defp tokens([h | t], accumulator, buffer, :text) do
     cond do
-      '"' === [h] -> tokens(t, read_buffer(buffer, :string) ++ accumulator, [], :common)
+      '"' === [h] -> tokens(t, [string(read_buffer(buffer))] ++ accumulator, [], :common)
       true -> tokens(t, accumulator, [h] ++ buffer, :text)
     end
   end
 
-  defp read_buffer([], _), do: []
+  defp read_buffer([]), do: []
 
-  defp read_buffer(buffer, kind) do
-    value = buffer |> Enum.reverse()
-    [%Token{kind: kind, value: value}]
+  defp read_buffer(buffer) do
+    buffer |> Enum.reverse()
   end
 end
--- lib/ego/token.ex
+++ lib/ego/token.ex
@@ -1,4 +1,12 @@
 defmodule Ego.Token do
   @enforce_keys [:kind, :value]
   defstruct [:kind, :value]
+
+  def open_bracket(), do: %Ego.Token{kind: :open_bracket, value: '('}
+  def close_bracket(), do: %Ego.Token{kind: :close_bracket, value: ')'}
+  def eof(), do: %Ego.Token{kind: :eof, value: ''}
+  def atom([]), do: []
+  def atom(value), do: %Ego.Token{kind: :atom, value: value}
+  def string([]), do: []
+  def string(value), do: %Ego.Token{kind: :string, value: value}
 end
