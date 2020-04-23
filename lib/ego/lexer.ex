d6ca2afb3f57e9c94f0101bd3ee87f8ac743c72f
Koziolek <bjkuczynski@gmail.com>
2020-04-23 23:48:31 +0200

    Numery i komentarze przed nagraniem

--- lib/ego/lexer.ex
+++ lib/ego/lexer.ex
@@ -1,6 +1,12 @@
 defmodule Ego.Lexer do
   import Ego.Token
 
+  defmacro is_new_line?(h) do
+    quote do
+      unquote([h]) == '\n'
+    end
+  end
+
   def tokenize(program) when is_binary(program) do
     program
     |> String.to_charlist()
@@ -10,7 +16,9 @@ defmodule Ego.Lexer do
   end
 
   defp tokens(charlist, accumulator \\ [], buffer \\ [], mode \\ :common)
-  defp tokens([], accumulator, _, _), do: [eof()] ++ accumulator
+
+  defp tokens([], accumulator, buffer, :number), do: [eof()] ++ [number(read_buffer(buffer))] ++ accumulator
+  defp tokens([], accumulator, buffer, _), do: [eof()] ++ [atom(read_buffer(buffer))] ++ accumulator
 
   defp tokens([h | t], accumulator, buffer, :common) do
     cond do
@@ -18,14 +26,30 @@ defmodule Ego.Lexer do
       ')' === [h] -> tokens(t, [close_bracket()] ++ [atom(read_buffer(buffer))] ++ accumulator, [])
       ' ' === [h] -> tokens(t, [atom(read_buffer(buffer))] ++ accumulator, [])
       '"' === [h] -> tokens(t, [atom(read_buffer(buffer))] ++ accumulator, [], :text)
+      ';' === [h] -> tokens(t, [atom(read_buffer(buffer))] ++ accumulator, [], :comment)
+      is_digit?(h) || is_sign?(h) -> tokens(t, accumulator, [h] ++ buffer, :number)
       true -> tokens(t, accumulator, [h] ++ buffer)
     end
   end
 
   defp tokens([h | t], accumulator, buffer, :text) do
     cond do
-      '"' === [h] -> tokens(t, [string(read_buffer(buffer))] ++ accumulator, [], :common)
+      '"' === [h] -> tokens(t, [string(read_buffer(buffer))] ++ accumulator, [])
       true -> tokens(t, accumulator, [h] ++ buffer, :text)
+    end
+  end
+
+  defp tokens([h | t], accumulator, _, :comment) when is_new_line?(h), do: tokens(t, accumulator, [])
+  defp tokens([_ | t], accumulator, _, :comment), do: tokens(t, accumulator, [], :comment)
+
+  defp tokens([h | t] = v, accumulator, buffer, :number) do
+    cond do
+      !has_only_digits(buffer, true, false) -> tokens(t, accumulator, [h] ++ buffer)
+      is_digit?(h) -> tokens(t, accumulator, [h] ++ buffer, :number)
+      '.' === [h] -> tokens(t, accumulator, [h] ++ buffer, :number)
+      ' ' === [h] && length(buffer) == 1 && is_sign?(buffer |> List.first) -> tokens(v, accumulator, buffer)
+      ' ' === [h] || ')' === [h] -> tokens(v, [number(read_buffer(buffer))] ++ accumulator, [])
+      true -> tokens(t, accumulator, [h] ++ buffer)
     end
   end
 
@@ -33,5 +57,23 @@ defmodule Ego.Lexer do
 
   defp read_buffer(buffer) do
     buffer |> Enum.reverse()
+  end
+
+  defp is_digit?(h) when h >= 48 and h <= 57, do: true
+  defp is_digit?(_), do: false
+  defp is_sign?(h) when h == 43 or h == 45, do: true
+  defp is_sign?(_), do: false
+  defp is_dot?(h) when h == 46, do: true
+  defp is_dot?(_), do: false
+
+  defp has_only_digits([], v, _), do: v
+  defp has_only_digits([h | _] = buffer, v, _) when length(buffer) == 1, do: (is_digit?(h) || is_sign?(h)) && v
+
+  defp has_only_digits([h | t], v, has_dot) do
+    if has_dot do
+      has_only_digits(t, is_digit?(h) && v, has_dot)
+    else
+      has_only_digits(t, (is_digit?(h) || is_dot?(h)) && v, is_dot?(h))
+    end
   end
 end
--- test/ego/lexer_test.exs
+++ test/ego/lexer_test.exs
@@ -90,4 +90,148 @@ defmodule Ego.LexerTest do
       %Token{kind: :eof, value: ''}
     ])
   end
+
+  test "Print" do
+    result = Lexer.tokenize("Print")
+
+    assert_lists_equal(result, [
+      %Token{kind: :atom, value: 'Print'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "; komentarz" do
+    result = Lexer.tokenize("; komentarz")
+
+    assert_lists_equal(result, [
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "1029" do
+    result = Lexer.tokenize("1029")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '1029'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "+1029" do
+    result = Lexer.tokenize("+1029")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '1029'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "-1029" do
+    result = Lexer.tokenize("-1029")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '-1029'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(1029)" do
+    result = Lexer.tokenize("(1029)")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :number, value: '1029'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(Print 1029)" do
+    result = Lexer.tokenize("(Print 1029)")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: 'Print'},
+      %Token{kind: :number, value: '1029'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(1029test)" do
+    result = Lexer.tokenize("(1029test)")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: '1029test'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(10+29)" do
+    result = Lexer.tokenize("(10+29)")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: '10+29'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(1029test1029)" do
+    result = Lexer.tokenize("(1029test1029)")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: '1029test1029'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "1.1" do
+    result = Lexer.tokenize("1.1")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '1.1'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "+1.1" do
+    result = Lexer.tokenize("+1.1")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '1.1'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "-1.1" do
+    result = Lexer.tokenize("-1.1")
+
+    assert_lists_equal(result, [
+      %Token{kind: :number, value: '-1.1'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
+
+  test "(print \"4+5\" (+ 4 5))" do
+    result = Lexer.tokenize("(print \"4+5\" (+ 4 5))")
+
+    assert_lists_equal(result, [
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: 'print'},
+      %Token{kind: :string, value: '4+5'},
+      %Token{kind: :open_bracket, value: '('},
+      %Token{kind: :atom, value: '+'},
+      %Token{kind: :number, value: '4'},
+      %Token{kind: :number, value: '5'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :close_bracket, value: ')'},
+      %Token{kind: :eof, value: ''}
+    ])
+  end
 end
